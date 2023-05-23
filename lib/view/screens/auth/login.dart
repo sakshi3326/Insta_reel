4f2ae0c518c11a748538c0237fb43c4cc5c6a0b9
sakshi3326 <saqkshiiitian123@gmail.com>
2023-05-24 01:23:37 +0530

    basic login screen

--- lib/view/screens/auth/login.dart
+++ lib/view/screens/auth/login.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+import '../../widgets/text_input.dart';
 
+class LoginScreen extends StatelessWidget {
+   LoginScreen({Key? key}) : super(key: key);
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const Text("Please Login"),
+        alignment: Alignment.center,
+        //const - Constant - Value - String , Int  - Fix Rahega  - Use Karna
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("InstaReel"),
+            SizedBox(height: 30),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _emailController,
+                myLabelText: "Email",
+                myIcon: Icons.email,
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _passwordController,
+                myLabelText: "Password",
+                myIcon: Icons.lock,
+                toHide: true,
+              ),
+            ),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed: (){
+            }, child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 50 , vertical: 10),
+
+                child: Text("Login"))),
+
+          ],
+        ),
+
       ),
     );
   }
